3aba77daa39e21ebb65df7eb56a5335d8220e35c
Mathias Green <flmathematic@gmail.com>
2017-12-04 02:28:26 +0100

    updated escript

--- src/ets_matrix.erl
+++ src/ets_matrix.erl
@@ -8,10 +8,29 @@
 %%====================================================================
 
 %% escript Entry point
-main(Args) ->
-    io:format("Args: ~p~n", [Args]),
-    erlang:halt(0).
-
+main([Module, Function, MatrixWidth, MatrixHeight, RunsNo]) ->
+    {Result, ok} = 
+	try  
+	    expected_time_estimation:run(
+	      list_to_atom(Module), 
+	      list_to_atom(Function), 
+	      list_to_integer(MatrixWidth),
+	      list_to_integer(MatrixHeight), 
+	      list_to_integer(RunsNo))
+	catch
+	    Mod:Error ->
+		io:format("~p~n",[[Mod,Error]]),
+		usage()
+	end,
+    io:format("~p~n",[Result]);
+main(_) ->
+    usage().
 %%====================================================================
 %% Internal functions
 %%====================================================================
+usage()->
+    io:format("usage: ets_matrix Module, Function, MatrixWidth, MatrixHeight, Number of runs \n"
+	      "Module   ex.  matrix_as_map\n"
+	      "Function ex.  get_value\n"
+	      "MatrixWidth, MatrixHeight, the number of runs are integers\n"
+              "examples: ets_matrix matrix_as_map get_value 1000 1000 1000 \n",[]).
